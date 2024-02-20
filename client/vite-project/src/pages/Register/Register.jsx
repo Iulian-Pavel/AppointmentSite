34400d2ed9ef06303d61d0345cc8f0e308e4447b
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2024-02-20 11:14:49 +0200

    Added error handling and rewrote axios.post

--- client/vite-project/src/pages/Register/Register.jsx
+++ client/vite-project/src/pages/Register/Register.jsx
@@ -7,34 +7,54 @@ function Register() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  const registerUsers = (e) => {
+  const registerUsers = async (e) => {
     e.preventDefault();
-    Axios.post("http://localhost:3001/insert", {username: username, email: email, password: password});
-  }
+
+    try {
+      await Axios.post("http://localhost:3001/insert", {
+        username: username,
+        email: email,
+        password: password,
+      });
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      setError("");
+    } catch (err) {
+      console.log(err);
+      if(err.response) {
+        setError(err.response.data);
+      } else {
+        setError("An error occured, please try again later");
+      }
+    }
+  };
 
   return (
     <>
-        <h1>Register</h1>
-        <input
-          type="text"
-          name="username"
-          id="username"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <input
-          type="email"
-          name="email"
-          id="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <button onClick={registerUsers}>Register</button>
+    <p>{error}</p>
+      <h1>Register</h1>
+      <input
+        type="text"
+        name="username"
+        id="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        name="password"
+        id="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        type="email"
+        name="email"
+        id="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button onClick={registerUsers}>Register</button>
     </>
   );
 }
