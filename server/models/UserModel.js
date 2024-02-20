90aee47774fd467ea3151f2cace065dc3c65fc8b
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2024-02-20 11:15:41 +0200

    updated email validation

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -5,6 +5,7 @@ let pattern;
 
 let validateEmail = (email) => {
     pattern = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    errorInfo = "Please enter a valid email address"
     return pattern.test(email);
 }
 
