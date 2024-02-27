f3827a09e6848d3e440573d3be694309e7a4b6a7
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2024-02-27 10:05:14 +0200

    added login functionality

--- server/index.js
+++ server/index.js
@@ -1,46 +1,71 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const UserModel = require('./models/UserModel');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const UserModel = require("./models/UserModel");
 const app = express();
-const bcrypt = require('bcrypt');
-require('dotenv').config();
-const port = 3001;
+const bcrypt = require("bcrypt");
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-let CONNECTION_LINK = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.hafyinl.mongodb.net/`
+let CONNECTION_LINK = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.hafyinl.mongodb.net/`;
 
 mongoose.connect(CONNECTION_LINK, {
-    usenewUrlParser: true
-})
-
-
-app.get('/', (req, res) => {
-    res.send("User logged in succesfully");
+  usenewUrlParser: true,
 });
 
-app.post('/insert', async (req, res) => {
-    
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
+app.get("/", (req, res) => {
+  res.send("User logged in succesfully");
+});
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+app.post("/insert", async (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
 
-    const user = new UserModel({Username: username, Email: email, Password: hashedPassword});
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    try {
-        await user.save();
-        console.log("User inserted succesfully");
-    }   
-    catch(err) {
-        console.error(`An error occured! ${err}`);
-        res.status(500).send("An error occured!" + err);
+  const user = new UserModel({
+    Username: username,
+    Email: email,
+    Password: hashedPassword,
+  });
+
+  try {
+    await user.save();
+    console.log("User inserted succesfully");
+  } catch (err) {
+    console.error(`An error occured! ${err}`);
+    res.status(500).send("An error occured!" + err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const userLogin = req.body.userLogin;
+  const passwordLogin = req.body.passwordLogin;
+
+  try {
+    const user = await UserModel.findOne({ Username: userLogin });
+
+    if (!user) {
+      console.log("user not found");
     }
-})
 
-app.listen(port, () => {
-    console.log(`Running on port ${port}`);
-})
+    const isPasswordValid = await bcrypt.compare(passwordLogin, user.Password);
+    if (!isPasswordValid) {
+      console.log("password is not valid");
+      res.status(500).send("password is not valid");
+      return;
+    } else {
+      console.log("password is valid");
+      console.log("login sucessful");
+    }
+  } catch (err) {
+    console.log(`Login error! ${err}`);
+  }
+});
+
+app.listen(3001, () => {
+  console.log(`Running on port 3001`);
+});
