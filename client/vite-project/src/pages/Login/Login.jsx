0055b1179ed45291a8f6a321b6f6041c24d26351
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2024-02-27 10:06:42 +0200

    added login functionality

--- client/vite-project/src/pages/Login/Login.jsx
+++ client/vite-project/src/pages/Login/Login.jsx
@@ -1,19 +1,48 @@
-import React, { useState } from 'react';
-import './login.scss';
+import React, { useState } from "react";
+import Axios from 'axios';
+import "./login.scss";
 
 function Login() {
+  const [userLogin, setUserLogin] = useState("");
+  const [passwordLogin, setPasswordLogin] = useState("");
+  const [errorlogin, setErrorLogin] = useState("");
 
-    const [userLogin, setUserLogin] = useState("");
-    const [passwordlogin, setPasswordLogin] = useState("");
-    const [errorlogin, setErrorLogin] = useState("");
+  const loginUser = async (e) => {
+    e.preventDefault();
+
+    try {
+      await Axios.post("http://localhost:3001/login", {
+        userLogin: userLogin,
+        passwordLogin: passwordLogin
+      });
+    } catch(err) {
+      if(err.response) {
+        setErrorLogin(err.response.data.message);
+      }
+      setErrorLogin("An error has occured, check your login information or try again later");
+    }
+  };
 
   return (
     <>
-        <input type="text" placeholder="username" name="username_login" id="username_login" />
-        <input type="password" placeholder="password" name="password_login" id="password_login" />
-        <button>Login</button>
+    <p>{errorlogin}</p>
+      <input
+        type="text"
+        placeholder="username"
+        name="userLogin"
+        id="userLogin"
+        onChange={(e) => setUserLogin(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        name="password_login"
+        id="password_login"
+        onChange={(e) => setPasswordLogin(e.target.value)}
+      />
+      <button onClick={loginUser}>Login</button>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
