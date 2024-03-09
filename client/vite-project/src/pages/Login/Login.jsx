be43c278064a0ca30d45b2b6f2e02e953aaf28b4
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2024-03-09 21:59:02 +0200

    added json response from server with the username

--- client/vite-project/src/pages/Login/Login.jsx
+++ client/vite-project/src/pages/Login/Login.jsx
@@ -1,31 +1,41 @@
-import React, { useState } from "react";
-import Axios from 'axios';
+import React, { useState, useContext } from "react";
+import Axios from "axios";
 import "./login.scss";
-
+import { Navigate, useNavigate } from "react-router-dom";
 function Login() {
+  const Navigate = useNavigate();
+
   const [userLogin, setUserLogin] = useState("");
   const [passwordLogin, setPasswordLogin] = useState("");
   const [errorlogin, setErrorLogin] = useState("");
+  //username after the user logs in
+  const [username, setUsername] = useState("");
 
   const loginUser = async (e) => {
     e.preventDefault();
 
     try {
-      await Axios.post("http://localhost:3001/login", {
+      const response = await Axios.post("http://localhost:3001/login", {
         userLogin: userLogin,
-        passwordLogin: passwordLogin
+        passwordLogin: passwordLogin,
       });
-    } catch(err) {
-      if(err.response) {
+      const data = response.data;
+      console.log(data.username);
+      Navigate(`/home/${data.username}`);
+    } catch (err) {
+      if (err.response) {
         setErrorLogin(err.response.data.message);
+      } else {
+        setErrorLogin(
+          "An error has occured, check your login information or try again later"
+        );
       }
-      setErrorLogin("An error has occured, check your login information or try again later");
     }
   };
 
   return (
     <>
-    <p>{errorlogin}</p>
+      <p>{errorlogin}</p>
       <input
         type="text"
         placeholder="username"
