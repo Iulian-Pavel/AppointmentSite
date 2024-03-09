dbdfd6eea351e337537751e7990c4886dee7c3d9
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2024-03-09 21:57:27 +0200

    added path to login and username

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,16 +1,19 @@
-import Register from "./pages/Register/Register"
-import Login from "./pages/Login/Login"
-import {Route, Routes} from "react-router-dom";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+import Home from "./pages/Home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
 
   return (
     <>
-      <Routes>
-      <Route path="/" element={<Register />}></Route>
-      <Route path="login" element={<Login />}></Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Register />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/home/:username" element={<Home />}></Route>
+        </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
