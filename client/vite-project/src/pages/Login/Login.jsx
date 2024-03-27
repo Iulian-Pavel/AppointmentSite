cb717bcfdc689e6f0c51f351eadd2b29140654d1
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2024-03-27 10:53:48 +0200

    added icons and better error information to the user

--- client/vite-project/src/pages/Login/Login.jsx
+++ client/vite-project/src/pages/Login/Login.jsx
@@ -2,14 +2,15 @@ import React, { useState, useContext } from "react";
 import Axios from "axios";
 import "./login.scss";
 import { Navigate, useNavigate } from "react-router-dom";
+import { MdOutlineAccountCircle } from "react-icons/md";
+import { GiPadlock } from "react-icons/gi";
+
 function Login() {
   const Navigate = useNavigate();
 
   const [userLogin, setUserLogin] = useState("");
   const [passwordLogin, setPasswordLogin] = useState("");
   const [errorlogin, setErrorLogin] = useState("");
-  //username after the user logs in
-  const [username, setUsername] = useState("");
 
   const loginUser = async (e) => {
     e.preventDefault();
@@ -20,11 +21,12 @@ function Login() {
         passwordLogin: passwordLogin,
       });
       const data = response.data;
+      console.log(data.error);
       console.log(data.username);
       Navigate(`/home/${data.username}`);
     } catch (err) {
       if (err.response) {
-        setErrorLogin(err.response.data.message);
+        setErrorLogin(err.response.data.error);
       } else {
         setErrorLogin(
           "An error has occured, check your login information or try again later"
@@ -35,22 +37,29 @@ function Login() {
 
   return (
     <>
+    <div className="login">
+      <h1>Logare</h1>
       <p>{errorlogin}</p>
+      <MdOutlineAccountCircle />
       <input
         type="text"
-        placeholder="username"
+        placeholder="Username"
         name="userLogin"
         id="userLogin"
+        className="user_login"
         onChange={(e) => setUserLogin(e.target.value)}
       />
+      <GiPadlock />
       <input
         type="password"
-        placeholder="password"
+        placeholder="Password"
         name="password_login"
         id="password_login"
+        className="password_login"
         onChange={(e) => setPasswordLogin(e.target.value)}
       />
       <button onClick={loginUser}>Login</button>
+      </div>
     </>
   );
 }
